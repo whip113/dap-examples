4e32f358a7e33237d8a2c63107d3266488897eac
Benjamin Floyd <benjamin.floyd@cyberark.com>
2020-02-21 15:05:26 -0600

    Adding README and updating policy_replace.sh with parameterized checks

--- scripts/policy_replace.sh
+++ scripts/policy_replace.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+if [ "$#" != "2" ];then
+    echo "Usage: $0 <branch> <policy_file>"
+    exit 1
+fi
+
 . authenticate.sh
 
-POLICY_FILE=root.yml
-POLICYBRANCH=root
-# ACCT=$ACCT
+POLICYBRANCH=$1
+POLICY_FILE=$2
 
 url="https://$URL/policies/$ACCT/policy/$POLICYBRANCH"
-curl -sk -H "$AUTH_TOKEN" \
+curl -s -k -H "$AUTH_TOKEN" \
   -X PUT \
   -d "$(< $POLICY_FILE )" \
   $url > policy-load.out
