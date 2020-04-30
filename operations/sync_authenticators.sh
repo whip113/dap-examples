967830d1e9520bf12d7ef102491bbbc36fe5507b
Ben Floyd <benjamin.floyd@cyberark.com>
2020-04-30 11:25:19 -0500

    Updating to make more friendly for cron

--- operations/sync_authenticators.sh
+++ operations/sync_authenticators.sh
@@ -1,9 +1,17 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 
-CONTAINER_NAME=dap
+ CONTAINER_NAME=dap
 
-docker exec $CONTAINER_NAME \
-  evoke variable set \
-  CONJUR_AUTHENTICATORS \
-  $(curl -sk https://localhost/info \
-    | jq -r '.authenticators.configured | join(",")')
+ enabled="$(docker exec $CONTAINER_NAME \
+     evoke variable list \
+     | grep CONJUR_AUTHENTICATORS | cut -f2 -d=)"
+
+ configured="$(curl -sk https://localhost/info \
+     | jq -r '.authenticators.configured | join(",")')"
+
+ if [ "$enabled" != "$configured" ]; then
+     docker exec $CONTAINER_NAME \
+         evoke variable set \
+         CONJUR_AUTHENTICATORS \
+         "$configured"
+ fi
