f52f336dd05aea712be7acc7630be151e6ab8c42
Ben Floyd <benjamin.floyd@cyberark.com>
2020-04-30 11:42:41 -0500

    Checking role in sync_authenticators.sh
    Checking the role to avoid unnecessary service thrashing

--- operations/sync_authenticators.sh
+++ operations/sync_authenticators.sh
@@ -1,17 +1,32 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
  CONTAINER_NAME=dap
 
- enabled="$(docker exec $CONTAINER_NAME \
-     evoke variable list \
-     | grep CONJUR_AUTHENTICATORS | cut -f2 -d=)"
+ CONT=1
 
- configured="$(curl -sk https://localhost/info \
-     | jq -r '.authenticators.configured | join(",")')"
+ INFO="$(curl -sk https://localhost/info)"
 
- if [ "$enabled" != "$configured" ]; then
-     docker exec $CONTAINER_NAME \
-         evoke variable set \
-         CONJUR_AUTHENTICATORS \
-         "$configured"
+ if [[ "$INFO" == *"\"role\": \"master\""* ]];then
+     CONT=0
+ elif [[ "$INFO" == *"\"role\": \"follower\""* ]];then
+     CONT=0
+ fi
+
+ if [ $CONT ]; then
+     echo "Role is master or follower..."
+     enabled="$(docker exec $CONTAINER_NAME \
+         evoke variable list \
+         | grep CONJUR_AUTHENTICATORS | cut -f2 -d=)"
+
+     configured="$(echo "$INFO" \
+         | jq -r '.authenticators.configured | join(",")')"
+
+     if [ "$enabled" != "$configured" ]; then
+         docker exec $CONTAINER_NAME \
+             evoke variable set \
+             CONJUR_AUTHENTICATORS \
+             "$configured"
+     fi
+ else
+     echo "Role is standby..."
  fi
